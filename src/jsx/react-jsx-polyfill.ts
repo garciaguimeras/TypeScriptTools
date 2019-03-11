2a5c86553b87f2b87dfc22fb0ed1d97019aafbeb
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2019-03-11 19:00:37 -0400

    Fixing copied-and-pasted code :)

--- src/jsx/react-jsx-polyfill.ts
+++ src/jsx/react-jsx-polyfill.ts
@@ -2,34 +2,33 @@
 
 class React {
 
-    static createElement(tag: any, attrs: any, ...children: any): Element {
+    static createElement(tag: any, attrs: any, ...children: any): Element | null {
 
         // Custom Components will be functions
         if (typeof tag === 'function') {
-            return tag()
+            return tag();
         }
-        
 
         // regular html tags will be strings to create the elements
         if (typeof tag === 'string') {
 
             // fragments to append multiple children to the initial node
-            const fragments = document.createDocumentFragment()
-            const element = document.createElement(tag)
+            const fragments = document.createDocumentFragment();
+            const element = document.createElement(tag);
 
             children.forEach(child => {
                 if (child instanceof HTMLElement) {
-                    fragments.appendChild(child)
+                    fragments.appendChild(child);
                 }
                 else if (typeof child === 'string') {
-                    const textnode = document.createTextNode(child)
-                    fragments.appendChild(textnode)
+                    const textnode = document.createTextNode(child);
+                    fragments.appendChild(textnode);
                 }
                 else {
                     // later other things could not be HTMLElement not strings
                     console.log('not appendable', child);
                 }
-            })
+            });
 
             element.appendChild(fragments)
 
@@ -42,7 +41,7 @@ class React {
                 });
             }
 
-            return element
+            return element;
         }
 
         return null;
