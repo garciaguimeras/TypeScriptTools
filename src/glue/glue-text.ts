5b0d58e8595c5819d850cae37b4a67d4d13c22ff
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2019-03-11 13:11:45 -0400

    Adding glue-text to inject strings and jsons

---
+++ src/glue/glue-text.ts
@@ -0,0 +1,33 @@
+﻿import { $glue } from './glue';
+
+function TextOutletTransformFunction(target: Element): string {
+    let text = target.getAttribute('value') || '';
+    return text;
+}
+
+function JsonOutletTransformFunction(target: Element): any {
+    let text = target.getAttribute('value') || '';
+    let obj: any = null;
+    try {
+        obj = JSON.parse(text);
+    }
+    catch { }
+    return obj
+}
+
+function TextOutlet(): any {
+    return function (target: any, name: string) {
+        $glue.newOutletTransformFunction(target, name, TextOutletTransformFunction);
+    }
+}
+
+function JsonOutlet(): any {
+    return function (target: any, name: string) {
+        $glue.newOutletTransformFunction(target, name, JsonOutletTransformFunction);
+    }
+}
+
+export {
+    TextOutlet,
+    JsonOutlet
+}
