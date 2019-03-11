17a8bdfaf96c5c77bc40d2c12167cfa599ea1b0c
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2019-03-11 13:12:14 -0400

    Adding react-jsx polyfill to allow using tsx components

---
+++ src/jsx/react-jsx-polyfill.ts
@@ -0,0 +1,55 @@
+﻿// Implementation from article: https://itnext.io/lessons-learned-using-jsx-without-react-bbddb6c28561
+
+class React {
+
+    static createElement(tag: any, attrs: any, ...children: any): Element {
+
+        // Custom Components will be functions
+        if (typeof tag === 'function') {
+            return tag()
+        }
+        
+
+        // regular html tags will be strings to create the elements
+        if (typeof tag === 'string') {
+
+            // fragments to append multiple children to the initial node
+            const fragments = document.createDocumentFragment()
+            const element = document.createElement(tag)
+
+            children.forEach(child => {
+                if (child instanceof HTMLElement) {
+                    fragments.appendChild(child)
+                }
+                else if (typeof child === 'string') {
+                    const textnode = document.createTextNode(child)
+                    fragments.appendChild(textnode)
+                }
+                else {
+                    // later other things could not be HTMLElement not strings
+                    console.log('not appendable', child);
+                }
+            })
+
+            element.appendChild(fragments)
+
+            // Merge element with attributes
+            // Object.assign(element, attrs)
+            if (attrs) {
+                Object.keys(attrs).forEach(key => {
+                    let value = attrs[key];
+                    element.setAttribute(key, value);
+                });
+            }
+
+            return element
+        }
+
+        return null;
+    }
+
+}
+
+export {
+    React
+}
