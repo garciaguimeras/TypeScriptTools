ff20f753c97712dcfd9af2b5a7590b4613cf3b55
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2019-04-01 12:30:11 -0400

    Adding new conversion functions to glue-text

--- src/glue/glue-text.ts
+++ src/glue/glue-text.ts
@@ -1,33 +1,85 @@
 ﻿import { $glue } from './glue';
 
-function TextOutletTransformFunction(target: Element): string {
-    let text = target.getAttribute('value') || '';
-    return text;
+function TextOutletTransformFunctionGenerator(attrName: string) {
+    return function (target: Element): string {
+        let text = target.getAttribute(attrName) || '';
+        return text;
+    };
 }
 
-function JsonOutletTransformFunction(target: Element): any {
-    let text = target.getAttribute('value') || '';
-    let obj: any = null;
-    try {
-        obj = JSON.parse(text);
-    }
-    catch { }
-    return obj
+function IntOutletTransformFunctionGenerator(attrName: string) {
+    return function (target: Element): number {
+        let text = target.getAttribute(attrName) || '';
+        return parseInt(text);
+    };
 }
 
-function TextOutlet(): any {
+function FloatOutletTransformFunctionGenerator(attrName: string) {
+    return function (target: Element): number {
+        let text = target.getAttribute(attrName) || '';
+        return parseFloat(text);
+    };
+}
+
+function JsonOutletTransformFunctionGenerator(attrName: string) {
+    return function (target: Element): any {
+        let text = target.getAttribute(attrName) || '';
+        let obj: any = null;
+        try {
+            obj = JSON.parse(text);
+        }
+        catch { }
+        return obj
+    };
+}
+
+function Base64JsonOutletTransformFunctionGenerator(attrName: string) {
+    return function (target: Element): any {
+        let text = target.getAttribute(attrName) || '';
+        text = atob(text);
+        let obj: any = null;
+        try {
+            obj = JSON.parse(text);
+        }
+        catch { }
+        return obj
+    };
+}
+
+function TextOutlet(attrName: string): any {
     return function (target: any, name: string) {
-        $glue.newOutletTransformFunction(target, name, TextOutletTransformFunction);
+        $glue.newOutletTransformFunction(target, name, TextOutletTransformFunctionGenerator(attrName));
     }
 }
 
-function JsonOutlet(): any {
+function IntOutlet(attrName: string): any {
     return function (target: any, name: string) {
-        $glue.newOutletTransformFunction(target, name, JsonOutletTransformFunction);
+        $glue.newOutletTransformFunction(target, name, IntOutletTransformFunctionGenerator(attrName));
+    }
+}
+
+function FloatOutlet(attrName: string): any {
+    return function (target: any, name: string) {
+        $glue.newOutletTransformFunction(target, name, FloatOutletTransformFunctionGenerator(attrName));
+    }
+}
+
+function JsonOutlet(attrName: string): any {
+    return function (target: any, name: string) {
+        $glue.newOutletTransformFunction(target, name, JsonOutletTransformFunctionGenerator(attrName));
+    }
+}
+
+function Base64JsonOutlet(attrName: string): any {
+    return function (target: any, name: string) {
+        $glue.newOutletTransformFunction(target, name, Base64JsonOutletTransformFunctionGenerator(attrName));
     }
 }
 
 export {
     TextOutlet,
-    JsonOutlet
+    IntOutlet,
+    FloatOutlet,
+    JsonOutlet,
+    Base64JsonOutlet
 }
