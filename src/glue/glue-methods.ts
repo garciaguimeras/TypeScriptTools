1814f73e90b3f8e2960c2d0d1087dd29f0bd254e
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2019-04-13 14:00:41 -0400

    Fixing glue-methods

--- src/glue/glue-methods.ts
+++ src/glue/glue-methods.ts
@@ -6,6 +6,10 @@ class GlueMethods {
         return $glue.getControllers(clazz).map<any>(c => c.instance);
     }
 
+    static getControllerNames(clazz: any): Array<string> {
+        return $glue.getControllers(clazz).map<any>(c => c.name);
+    }
+
     static getControllerCount(clazz: any): number {
         return $glue.getControllers(clazz).length;
     }
@@ -20,28 +24,46 @@ class GlueMethods {
         $glue.removeController(name);
     }
 
-    static templateToController(parentControllerName: string, templateClazz: any, id: string, parentId: string, initAttributes?: any): any {
-        let controller = $glue.getController(parentControllerName);
-        if (!controller)
+    static removeControllerInstance(clazz: any, instance: any) {
+        let controllers = $glue.getControllers(clazz).filter(c => c.instance == instance);
+        if (controllers == null || controllers.length == 0)
+            return;
+
+        let controller = controllers[0];
+        if (controller.element.parentElement)
+            controller.element.parentElement.removeChild(controller.element);
+        $glue.removeController(controller.name);
+    }
+
+    static templateToController(parentElement: string | Element, templateClazz: any, id: string, parentId: string, initAttributes?: any): any {
+        let parent: Element | null = null;
+
+        if (typeof parentElement === 'string') {
+            let controller = $glue.getController(parentElement);
+            if (!controller)
+                return;
+            parent = controller.element;
+        }
+        else
+            parent = parentElement as Element;
+
+        parent = parent.querySelector('#' + parentId);
+        if (!parent)
             return;
 
         let template = $glue.getTemplate(templateClazz);
         if (!template)
             return;
 
-        let parentElement = controller.element.querySelector('#' + parentId);
-        if (!parentElement)
-            return;
-
         let elem = template.element.cloneNode(true) as Element;
         elem.removeAttribute('id');
         elem.setAttribute('id', id);
-        parentElement.appendChild(elem);
+        parent.appendChild(elem);
 
         let newController = $glue.newController(id, template.clazz);
         if (!newController)
             return;
-            
+          
         $glue.populateController(newController, initAttributes);
         $glue.notifyLoad(newController);
 
